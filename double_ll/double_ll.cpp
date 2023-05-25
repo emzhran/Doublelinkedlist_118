7a5979f08224fa4110ec74feb683b34d4edc18ca
emzhran <zachran750@gmail.com>
2023-05-25 20:15:02 +0700

    void DoubleLikedList traverse

--- double_ll/double_ll.cpp
+++ double_ll/double_ll.cpp
@@ -101,3 +101,20 @@ bool DoubleLinkedList::deleteNode(int rollNo) {
 	delete current;							//step 4
 	return true;
 }
+
+bool DoubleLinkedList::listEmpty() {
+	return (START == NULL);
+}
+
+void DoubleLinkedList::traverse() {
+	if (listEmpty())
+		cout << " \nList is empty" << endl;
+	else {
+		cout << " \nRecords is ascending order of roll number are: " << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout <<  currentNode-> noMhs << "" << currentNode->name <<endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
